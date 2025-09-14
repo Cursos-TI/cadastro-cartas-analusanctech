e1d5f50ecd9897b3ca590f80b21cd6088ba75914
analusanctech <analusanc1@gmail.com>
2025-09-14 16:44:21 +0000

    variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,26 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado1
+  char codigo1[3]
+  char cidade1[20]
+  int população1
+  float area1 
+  float PIB1
+  int turisticos1
+
+
 
   // Área para entrada de dados
+  printf("Digite o Estado: ");
+  printf("Digite o código: ");
+  printf("Digite a cidade: ");
+  printf("Digite a populaçao: ");
+  printf("Digite a área: ");
+  printf("Digite o PIB: ");
+  printf("Digite o número de pontis turísticos: ");
+  
+
 
   // Área para exibição dos dados da cidade
 
